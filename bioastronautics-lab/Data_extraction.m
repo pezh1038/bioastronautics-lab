0949072da045d512012bae23520aa163f5b1de4e
pezh1038 <pezh1038@colorado.edu>
2026-01-22 10:00:44 -0700

    Enhance Data Extraction in Data_extraction.m
    Added data extraction and processing for voltage and time.

--- bioastronautics-lab/Data_extraction.m
+++ bioastronautics-lab/Data_extraction.m
@@ -8,3 +8,22 @@ data2SpringF = readtable("Muslce_LA_Data_2spring_Calf_Raise_Fast.txt");
 data2SpringS = readtable("Muslce_LA_Data_2spring_Calf_Raise_Slow.txt");
 data3SpringF = readtable("Muslce_LA_Data_3spring_Calf_Raise_Fast.txt");
 data3SpringS = readtable("Muslce_LA_Data_3spring_Calf_Raise_Slow.txt");
+
+time1 = data1.Time_s_;
+voltage1 = data1.Voltage_V_;
+
+lenth = length(voltage1);
+
+actV = [0];
+actT = [0];
+
+for i = 1:lenth
+    if(voltage1(i) > 0.12)
+        actV(i) = voltage1(i);
+        actT(i) = time1(i);
+    end
+end
+
+   
+
+plot(time1,voltage1);
