5cac0710190cffadf50b1f04307db2ac0811dd21
pezh1038 <pezh1038@colorado.edu>
2026-01-22 09:46:03 -0700

    Add files via upload

---
+++ bioastronautics-lab/Data_extraction.m
@@ -0,0 +1,10 @@
+clc, close all, clear;
+
+data1 = readtable("Muslce_LA_Data_1-g_Calf_Raise_Fast.txt");
+data2 = readtable("Muslce_LA_Data_1-g_Calf_Raise_Slow.txt");
+data1SpringF = readtable("Muslce_LA_Data_1spring_Calf_Raise_Fast.txt");
+data1SpringS = readtable("Muslce_LA_Data_1spring_Calf_Raise_Slow.txt");
+data2SpringF = readtable("Muslce_LA_Data_2spring_Calf_Raise_Fast.txt");
+data2SpringS = readtable("Muslce_LA_Data_2spring_Calf_Raise_Slow.txt");
+data3SpringF = readtable("Muslce_LA_Data_3spring_Calf_Raise_Fast.txt");
+data3SpringS = readtable("Muslce_LA_Data_3spring_Calf_Raise_Slow.txt");
