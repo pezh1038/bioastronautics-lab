72669a03f2f684f398c96ab7c45da51528225c28
pezh1038 <pezh1038@colorado.edu>
2026-01-22 10:20:59 -0700

    Enhance data extraction with voltage categorization
    Added functionality to categorize and analyze voltage readings based on time intervals.

--- bioastronautics-lab/Data_extraction.m
+++ bioastronautics-lab/Data_extraction.m
@@ -14,20 +14,40 @@ voltage1 = data1.Voltage_V_;
 
 lenth = length(voltage1);
 
-actV = [0];
-actT = [0];
+
+actNum = 1;
+actV1 = [0];
+actT1 = [0];
+actV2 = [0];
+actT2 = [0];
+actV3 = [0];
+actT3 = [0];
 
 for i = 1:lenth
     if(voltage1(i) > 0.12)
-        actV(i) = voltage1(i);
-        actT(i) = time1(i);
+        if(time1(i) > 1 && time1(i) < 3)
+            actV1(i) = voltage1(i);
+            actT1(i) = time1(i);
+        elseif(time1(i) > 3 && time1(i) < 5)
+            actV2(i) = voltage1(i);
+            actT2(i) = time1(i);
+        elseif(time1(i) > 5 && time1(i) < 8)
+            actV3(i) = voltage1(i);
+            actT3(i) = time1(i);
+        end
     end
 end
 
+M1 = mean(actV1);
+M2 = mean(actV2);
+M3 = mean(actV3);
+SD1 = std(actV1);
+SD2 = std(actV2);
+SD3 = std(actV3);
+
+R1 = range(actV1);
+R2 = range(actV2);
+R3 = range(actV3);
    
 
 plot(time1,voltage1);
-
-
-RAHHHHH
-
